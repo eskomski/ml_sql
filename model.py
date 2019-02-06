40fd2dbafd43f3f4582d4c465fd9884d90b79ac0
skomski <skomsks@wwu.edu>
2019-02-06 00:00:10 -0800

    some optimizations, filled out analysis notebook

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
+'''
+model.py: a DNN trained on MNIST with a training routine that stores per-epoch
+results in a SQLite3 database. This is mostly boilerplate; the code of interest
+appears at the end of the run_exp function.
+
+Elliott Skomski (skomsks@wwu.edu)
+'''
+
 import sqlite3
 
 from torch import nn, optim
@@ -5,6 +13,7 @@ from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import MNIST
 from torchvision.transforms.functional import to_tensor
 
+# MNIST loading/splitting
 data = MNIST('data', train=True, transform=to_tensor, download=True)
 train_size = int(len(data)*0.7)
 split = random_split(data, [train_size, len(data) - train_size])
@@ -12,6 +21,7 @@ split = random_split(data, [train_size, len(data) - train_size])
 train = DataLoader(split[0], batch_size=4096)
 dev = DataLoader(split[1], batch_size=4096)
 
+# standard DNN w/ ReLU activation
 class DNN(nn.Module):
     def __init__(self, d, c, layers=[128, 64]):
         super(DNN, self).__init__()
@@ -28,9 +38,20 @@ class DNN(nn.Module):
             x = layer(x).clamp(min=0)
         return self.layers[-1](x)
 
-def run_exp(layers, lr, conn=None, exp_id=None):
-    if conn and exp_id is None:
-        print('error: you forgot the experiment id. incrementing bozo counter...')
+def run_exp(layers, lr, cur=None, exp_id=None):
+    '''
+    Runs an experiment on MNIST with the given layer sizes and learning rate.
+    Optionally interacts with a SQLite3 DB given a cursor and experiment ID.
+
+    Parameters:
+        layers (list of ints): a list of layer sizes
+        lr (float): learn rate
+        cur (sqlite3.Cursor): cursor obtained from DB connection made by caller
+        exp_id (int): unique identifier for experiment
+    '''
+
+    if cur and exp_id is None:
+        print('error: experiment ID required for DB interaction.')
         return
 
     model = DNN(28*28, 10, layers)
@@ -38,9 +59,7 @@ def run_exp(layers, lr, conn=None, exp_id=None):
     criterion = nn.CrossEntropyLoss()
     opt = optim.Adam(model.parameters(), lr=lr)
 
-    if conn: c = conn.cursor()
-
-    for e in range(5):
+    for e in range(50):
         model.train()
         train_loss = 0.0; train_acc = 0
 
@@ -72,7 +91,9 @@ def run_exp(layers, lr, conn=None, exp_id=None):
         print('epoch {}: train loss={:.4f} acc={:.4f}, dev loss={:.4f} acc={:.4f}'
                 .format(e, train_loss, train_acc, dev_loss, dev_acc))
 
-        if conn:
-            c.execute('insert into results values (?, ?, ?, ?)', (exp_id, e, dev_loss, dev_acc))
-            conn.commit()
+        # after each epoch, insert losses into results table
+        if cur:
+            cur.execute('insert into results values (?, ?, ?, ?, ?, ?)',
+                      (exp_id, e, train_loss, train_acc, dev_loss, dev_acc))
+            cur.connection.commit()   # commit changes asap to avoid locking DB for too long
 
--- run_exps.py
+++ run_exps.py
@@ -1,32 +1,47 @@
+'''
+run_exps.py: a simple experiment script that establishes a connection with a
+SQLite3 database, then performs a random hyperparameter search.
+
+Elliott Skomski (skomsks@wwu.edu)
+'''
+
 import sqlite3
 from random import randint, uniform
 
 import model
 
-# create database
-conn = sqlite3.connect('exps.db')
-c = conn.cursor()
-c.execute('create table if not exists experiments (exp_id integer, layers integer, layersizes text, lr real)')
-c.execute('create table if not exists results (exp_id integer, epoch integer, val_loss real, val_acc real)')
-conn.commit()
+conn = sqlite3.connect('exps.db') # create DB or load if it already exists
 
-# generate random hyperparameters
-num_exps = 1000
-layer_cfgs = [[randint(32, 128) for i in range(randint(1, 3))] for j in range(num_exps)]
-learning_rates = [uniform(0.003, 0.045) for i in range(num_exps)]
+# get cursor to make queries, then add necessary tables if they don't exist
+cur = conn.cursor()
+cur.execute('create table if not exists experiments (exp_id integer, layers integer, \
+           layersizes text, lr real)')
+cur.execute('create table if not exists results (exp_id integer, epoch integer, \
+           train_loss real, train_acc real, val_loss real, val_acc real)')
+cur.connection.commit()
+
+# get largest experiment ID from DB
+cur.execute('select max(exp_id) from experiments')
+exp_id = cur.fetchone()[0]
+exp_id = 0 if exp_id is None else exp_id + 1
 
 # experiment loop
-for layers, lr in zip(layer_cfgs, learning_rates):
-    c.execute('select max(exp_id) from experiments')
+num_exps = 1000
+for _ in range(num_exps):
+    # generate random hyperparameters
+    layers = [randint(32, 128) for i in range(randint(1, 3))]
+    lr = uniform(0.003, 0.045)
 
-    # increment experiment id, then add new row to experiments table
-    exp_id = c.fetchone()[0]
-    exp_id = 1 + exp_id if exp_id is not None else 0
-    c.execute('insert into experiments values (?, ?, ?, ?)', (exp_id, len(layers), ' '.join(map(str, layers)), lr))
-    conn.commit()
+    # insert new experiment info into DB
+    cur.execute('insert into experiments values (?, ?, ?, ?)',
+              (exp_id, len(layers), ' '.join(map(str, layers)), lr))
+    cur.connection.commit()
 
+    # run the experiment
     print('running experiment {}: {}, {}'.format(exp_id, layers, lr))
-    model.run_exp(layers, lr, conn, exp_id)
+    model.run_exp(layers, lr, cur, exp_id)
+
+    exp_id += 1
 
 conn.close()
 
